c86cfe7261a31980910c4e66036235ec8facaa5c
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-28 16:08:55 +0530

    delaring variables in go

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Enjoy Learning Go"
+	fmt.Println(whatToSay)
+
+	i = 10
+	fmt.Println(i)
 }
