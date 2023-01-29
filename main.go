494bc93dba7cea8550ae576f2d0e6d2d0606b227
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-29 23:39:24 +0530

    description about channel

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/aak-301/myniceprogram/helper"
 )
 
+// It is used fo concurrency i Go
+// It is used to send data from one package to another
+
 const numPool = 1000
 
 func CalculateValue(intChan chan int) {
 	randomNumber := helper.RandomNumber(numPool)
+	// writing in intChan(channel)
 	intChan <- randomNumber
 }
 
@@ -19,6 +23,7 @@ func main() {
 
 	go CalculateValue(intChan)
 
+	// reading from intChan(channel)
 	num := <-intChan
 	log.Println(num)
 }
