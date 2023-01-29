f676c6a6093bd1d9924f9ec9f582306b585dfc41
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-29 15:14:05 +0530

    empty main() in main

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World")
 
-	var whatToSay string
-	var i int
-
-	whatToSay = "Enjoy Learning Go"
-	fmt.Println(whatToSay)
-
-	i = 10
-	fmt.Println(i)
-
-	// := makes the variable dynamic type which detects its datatype on the fly.
-	whatWasSaid := saySomehing()
-	fmt.Println(whatWasSaid)
-
-	returnMany1, returnMany2 := returnManyThingsFromSameFunction()
-	fmt.Println(returnMany1, returnMany2)
-}
-
-func saySomehing() string {
-	return "something"
-}
-
-func returnManyThingsFromSameFunction() (string, string) {
-	return "return 1st object\n", "return 2nd object"
 }
