e2418e027ff7e06a8a8f90522c6c41d871509c0a
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-30 00:32:15 +0530

    testing ex

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"log"
+)
 
+func main() {
+	result, err := divide(100, 0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("result of division is", result)
+}
+
+func divide(x, y float32) (float32, error) {
+	var result float32
+	if y == 0 {
+		return result, errors.New("cannot divide by 0")
+	}
+	result = x / y
+	return result, nil
 }
---
+++ main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import "testing"
+
+/*
+Things you can do:
+1) Check your coverage with this command:
+    go test -cover
+
+2) Get your coverage in the browser with this command:
+    go test -coverprofile=coverage.out && go tool cover -html=coverage.out
+*/
+
+// manual tests
+func TestDivide(t *testing.T) {
+	_, err := divide(10.0, 1.0)
+	if err != nil {
+		t.Error("Got an error when should not have")
+	}
+}
+
+func TestBadDivide(t *testing.T) {
+	_, err := divide(10.0, 0)
+	if err == nil {
+		t.Error("Did not get an error when should have")
+	}
+}
+
+// table test
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isErr    bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"expect-5", 50.0, 10.0, 5.0, false},
+	{"expect-fraction", -1.0, -777.0, 0.0012870013, false},
+}
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+		if tt.isErr {
+			if err == nil {
+				t.Error("Expected an error but did not get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("Did not expect an error, but got one", err.Error())
+			}
+		}
+
+		if got != tt.expected {
+			t.Errorf("Expected %f but got %f", tt.expected, got)
+		}
+	}
+}
