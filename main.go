8f7be9428e4034f86a5ae298a089eb3c07ff6350
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-29 09:05:59 +0530

    slices in go lang

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sort"
 )
 
 // Maps are immutable
@@ -35,4 +36,27 @@ func main() {
 	userMap["first"] = userData
 	log.Println(userMap["first"].FirstAme, userMap["first"].LastName)
 
+	// slices
+
+	var myString string
+	myString = "Fish"
+	log.Println(myString)
+
+	var myString2 []string
+	myString2 = append(myString2, "Aakash")
+	myString2 = append(myString2, "Shivanshu")
+	log.Println(myString2)
+
+	var myString3 []int
+	myString3 = append(myString3, 3)
+	myString3 = append(myString3, 1)
+	myString3 = append(myString3, 2)
+	log.Println(myString3)
+
+	sort.Ints(myString3)
+	log.Println(myString3)
+
+	numbers := []int{1, 2, 3, 7, 5, 9, 0}
+	log.Println(numbers)
+	log.Println(numbers[0:2])
 }
