7a6956840a0f71ed955095823e4069910a4a6815
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-29 07:39:42 +0530

    function in structure

--- main.go
+++ main.go
@@ -16,12 +16,33 @@ type User struct {
 	BirthDate  time.Time
 }
 
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) printFirstNAme() string {
+	return m.FirstName
+}
+
 func main() {
-	user := User{
-		FirstName:  "Aakash",
-		LastName:   "Shivanshu",
-		PoneNumber: "+91-1234567890",
+	// user := User{
+	// 	FirstName:  "Aakash",
+	// 	LastName:   "Shivanshu",
+	// 	PoneNumber: "+91-1234567890",
+	// }
+
+	// log.Println(user.FirstName)
+
+	var myVar myStruct
+	myVar.FirstName = "Aman"
+
+	myVar2 := myStruct{
+		FirstName: "Ashish",
 	}
 
-	log.Println(user.FirstName)
+	// log.Println("myVar= ", myVar.FirstName)
+	// log.Println("myVar2= ", myVar2.FirstName)
+
+	log.Println("myVar= ", myVar.printFirstNAme())
+	log.Println("myVar2= ", myVar2.printFirstNAme())
 }
